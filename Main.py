e017030a798f4bff36a3ebaf67190be8eff489dc
Vignesh Ganesan <Vignesh1296@users.noreply.github.com>
2017-10-09 23:01:59 +0530

    MainFrame

---
+++ Main.py
@@ -0,0 +1,21 @@
+#DBMS PROJECT
+#CREATED BY VIGNESH (RA1511003010323)
+#15IT302J
+
+from tkinter import *
+import sqlite3
+import tkinter.ttk as ttk
+
+def Main():
+    root = Tk()
+    w = 450
+    h = 300
+    ws = root.winfo_screenwidth()  # width of the screen
+    hs = root.winfo_screenheight()  # height of the screen
+    x = (ws / 2) - (w / 2)
+    y = (hs / 2) - (h / 2)
+    root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    root.title("Database Mangaement System Project")
+    root.mainloop()
+
+Main()
